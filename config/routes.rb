c4ea17a5f32ad0217a2593d420c5598a3ff8d01c
yhorie517 <yhkovoodoo@gmail.com>
2019-02-14 20:12:40 +0900

    [Add]サインアップとログアウト機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/index'
   get 'home/top'
   get 'home/about'
   root to: 'home#top'
   devise_for :users
+  resources :books, only: [:new, :show, :index, :create]
+  resources :users, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
